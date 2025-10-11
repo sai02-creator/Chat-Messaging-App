f51ab7eafd0022e68ce58db83f108c00bfa21ffb
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-10-12 00:25:20 +1100

    Implement Navbar with Chat Room, Rooms, Create Room links and Logout button using Supabase

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+import { supabase } from "../supabaseClient";
+
+
+function Navbar() {
+    const handleLogout = async () => {
+        await supabase.auth.signOut();
+    }
+    return <nav className="navbar">
+        <ul className="clearfix">
+            <li>
+                <Link to="/"> Chat Room </Link>
+            </li>
+            <li>
+                <Link to="/rooms"> Available Rooms </Link>
+            </li>
+            <li>
+                <Link to="/create-room"> Create Room </Link>
+            </li>
+        </ul>
+        <button className="logout-button" onClick={handleLogout}> Logout </button>
+        </nav>;
+}
+
+export default Navbar;
