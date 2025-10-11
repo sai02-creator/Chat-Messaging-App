b58a3d666f1389b9ee53bb095ab3061e47487d49
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-10-12 01:00:49 +1100

    Integrate ChatMessages and ChatMessageForm into ChatRoom for message display and input

---
+++ src/pages/ChatRoom.tsx
@@ -0,0 +1,25 @@
+import ChatMessages from "../components/ChatMessages";
+import ChatMessageForm from "../components/ChatMessageForm"; // added import
+import { useChatStore } from "../store/chatStore";
+
+function ChatRoom() {
+    const { user, currentRoom } = useChatStore();
+
+    if (!currentRoom) {
+        return <p> Please join or create a room first.</p>
+    }
+    return(
+        <div className="conv">
+            <div className="conv-title"> {currentRoom.name} - {user?.email}</div>
+            <div className="conv-timeline">
+               <ChatMessages />
+            </div>
+
+            <div className="conv-send-message">
+                <ChatMessageForm />
+            </div>
+        </div>
+    );
+}
+
+export default ChatRoom;
